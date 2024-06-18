fc96d965c453e0b995f1584caae32434623ec3c4
saurabh90-tech <sadakhath07@gmail.com>
2024-06-18 14:15:25 +0530

    DSC sample

---
+++ datastructures/Test1.java
@@ -0,0 +1,20 @@
+package datastructures;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] marks;
+		marks = new int[5];
+		marks[0] = 85;
+		marks[1] = 75;
+		marks[2] = 95;
+		marks[3] = 65;
+		marks[4] = 55;
+		
+		for(int i = 0; i < marks.length; i++) {
+			System.out.println(i + " " + marks[i]);
+		}
+	}
+
+}
